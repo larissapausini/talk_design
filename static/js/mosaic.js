52634b2bb3496a31605604e6af923a05ac00e7e7
Leandro Casanova <casanova.leandro@gmail.com>
2016-05-12 22:21:05 -0300

    Added clean version

--- static/js/mosaic.js
+++ static/js/mosaic.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 function getRandomSize(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
-  for (var i = 0; i < 5; i++) {
-    var width = getRandomSize(260, 555);
-    var height =  getRandomSize(200, 500);
+  for (var i = 0; i < 10; i++) {
+    var width = getRandomSize(230, 230);
+    var height =  getRandomSize(200, 400);
     $('#photos').append('<img src="http://www.lorempixel.com/'+width+'/'+height+'/city" alt="London">');
   }
 });
