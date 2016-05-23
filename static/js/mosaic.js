317838d240a5ca95c7f1966cb524513b6a1e0198
Leandro Casanova <casanova.leandro@gmail.com>
2016-05-22 23:24:45 -0300

    Novo layout

--- static/js/mosaic.js
+++ static/js/mosaic.js
@@ -3,8 +3,8 @@ function getRandomSize(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
   for (var i = 0; i < 10; i++) {
-    var width = getRandomSize(230, 230);
-    var height =  getRandomSize(200, 400);
+    var width = getRandomSize(231, 231);
+    var height =  getRandomSize(150, 340);
     $('#photos').append('<img src="http://www.lorempixel.com/'+width+'/'+height+'/city" alt="London">');
   }
 });
