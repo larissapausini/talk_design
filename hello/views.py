0781989d3759e8a5b47d687655383899de617edc
Leandro Casanova <casanova.leandro@gmail.com>
2016-06-16 22:10:45 -0300

    Migrate pacotes page to django template

--- hello/views.py
+++ hello/views.py
@@ -5,16 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
+def pacotes(request):
+    return render(request, 'pacotes.html')
